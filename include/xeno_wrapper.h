7f1d686fc6bdf145ffafa865df448e719d61aaf6
hmgqkh-wq <hmgqkh@gmail.com>
2025-11-18 00:35:25 +0330

    Create xeno_wrapper.h

---
+++ include/xeno_wrapper.h
@@ -0,0 +1,13 @@
+#ifndef XENO_WRAPPER_H
+#define XENO_WRAPPER_H
+
+#include <stdint.h>
+
+void xeno_init(void);
+void xeno_open_log(void);
+void xeno_log(const char *fmt, ...);
+void xeno_log_event(const char *json);
+void xeno_log_bin(const char *relpath, const void *data, size_t size);
+void xeno_flush_all(void);
+
+#endif
